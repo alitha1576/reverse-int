28077bda05c36658d7c198d5b868531d4cff8034
Anna Petrova <ann.undine@gmail.com>
2024-08-19 10:08:26 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let arr = Array.from(n.toString());
+    let newNumber = '';
+    while (arr.length > 0) {
+        let lastElement = arr.pop();
+        newNumber += lastElement;
+    }
+    return parseInt(newNumber);
 }
